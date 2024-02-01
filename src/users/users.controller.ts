c45cd0eee3b6224b17c82e91467e596361fb2ee7
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2024-02-01 14:59:02 +0100

    modified

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: String) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: String, @Body() user: {}) {
+    return { id, user };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: String) {
+    return { id };
+  }
+}
